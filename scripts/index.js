a472f331553e990fc29003f342ffea5a6e94726c
Fedor Nikolaev <nikolaevfo@gmail.com>
2020-11-07 12:38:18 +0400

    feat/добавить генерацию 6 картинок из джавы

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,46 @@ let jobInput = document.querySelector('.popup__text_type_profession');
 let name = document.querySelector('.profile__title');
 let job = document.querySelector('.profile__subtitle');
 
+const initialCards = [
+  {
+    name: 'Зуевы Ключи',
+    link: './images/zuevi-cluchi.jpg'
+  },
+  {
+    name: 'Чегандинские пещеры',
+    link: './images/chegadinskie-pesheri.jpg'
+  },
+  {
+    name: 'Заброшенный храм',
+    link: './images/hram.jpg'
+  },
+  {
+    name: 'Мувыр',
+    link: './images/muvir.jpg'
+  },
+  {
+    name: 'Пруд в Коробейниках',
+    link: './images/prud-corobejniki.jpg'
+  },
+  {
+    name: 'Сидоровы горы',
+    link: './images/sidorovi-gori.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.elements');
+
+const cardElements = initialCards.map(function (item) {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = item.name;
+  cardElement.querySelector('.card__img').src = item.link;
+  return cardElement
+})
+
+cardsContainer.append(...cardElements);
+
+
 function openPopup() {
   nameInput.value = name.textContent;
   jobInput.value = job.textContent;
